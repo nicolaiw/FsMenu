cae9d060737c8b47f7ff46beab333b29ae1d46fb
Wirtz.Nicolai@kermi.de <WIRTZNI@afg.ads>
2017-07-21 10:44:38 +0200

    typ;prettyPrint

--- FsMenu/Program.fs
+++ FsMenu/Program.fs
@@ -5,18 +5,19 @@ open Menu
 [<EntryPoint>]
 let main argv =
     
-    printfn "Us Keys: UP-Arrow DOWN-Arrow ENTER BACK-SPACE\n"
+    printfn "Use Keys: UP-Arrow DOWN-Arrow ENTER BACK-SPACE\n"
 
-    let test = Menu [
-                     "Item 1" => (fun () -> printf "selected Item 1")
-                     "Item 2" +>
-                        Menu [ 
-                            "Sub 1" +>
-                                    Menu [
-                                        "Sub Sub 1" => (fun () -> printf "selected Sub Sub 1")
-                                        "Sub Sub 2" => (fun () -> printf "selected Sub Sub 2")
-                                        "Sub Sub 3" => (fun () -> printf "selected Sub Sub 3")]
-                            "Sub 2" => (fun () -> printf "selected Sub 2") ]]
+    let test =
+        Menu [
+            "Item 1" => (fun () -> printf "selected Item 1")
+            "Item 2" +>
+               Menu [ 
+                   "Sub 1" +>
+                           Menu [
+                               "Sub Sub 1" => (fun () -> printf "selected Sub Sub 1")
+                               "Sub Sub 2" => (fun () -> printf "selected Sub Sub 2")
+                               "Sub Sub 3" => (fun () -> printf "selected Sub Sub 3")]
+                   "Sub 2" => (fun () -> printf "selected Sub 2") ]]
 
     render test "<--"
     
